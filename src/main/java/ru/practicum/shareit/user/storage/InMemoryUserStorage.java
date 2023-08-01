219ac82f34c70b1c7205fcbc1e13b581b616d319
Aleksandr <sas0501@yandex.ru>
2023-08-01 23:53:53 +0300

    User Item реализованы

--- src/main/java/ru/practicum/shareit/user/storage/InMemoryUserStorage.java
+++ src/main/java/ru/practicum/shareit/user/storage/InMemoryUserStorage.java
@@ -40,9 +40,8 @@ public class InMemoryUserStorage implements UserStorage {
 
     @Override
     public UserDto update(Long id, UserDto userDto) {
-        User userWithMatchingEmail = listUsers.values()
-                .stream().filter(user -> user.getEmail().equals(userDto.getEmail()) && !user.getId().equals(id))
-                .findFirst().orElse(null);
+        User userWithMatchingEmail = listUsers.values().stream().filter(user -> user.getEmail().
+                equals(userDto.getEmail()) && !user.getId().equals(id)).findFirst().orElse(null);
         if (userWithMatchingEmail != null) {
             throw new RuntimeException(String.format("Такой email  %s уже есть у другого пользователя ", userDto.getEmail()));
         }
